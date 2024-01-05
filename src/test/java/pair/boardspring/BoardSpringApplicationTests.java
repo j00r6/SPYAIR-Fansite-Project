645188389306b2d3d12bc08beb1ab20318255ced
j00r6 <jindory8@gmail.com>
2024-01-05 21:14:52 +0900

    basic setting

---
+++ src/test/java/pair/boardspring/BoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pair.boardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
