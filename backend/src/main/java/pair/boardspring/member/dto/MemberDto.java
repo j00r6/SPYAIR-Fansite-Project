d4218e23278548540862e2b2081901e47f969725
j00r6 <jindory8@gmail.com>
2024-01-08 22:32:24 +0900

    Member Patch Del

--- backend/src/main/java/pair/boardspring/member/dto/MemberDto.java
+++ backend/src/main/java/pair/boardspring/member/dto/MemberDto.java
@@ -33,44 +33,4 @@ public class MemberDto {
         public String email;
         public String password;
     }
-
-    // 마이페이지 접근 Or 회원정보 조히 시
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    public static class memberInfoRequest {
-        public String email;
-        public String password;
-        public String nickName;
-        public String phone;
-        public String name;
-        // 게시글 리스트 추가
-    }
-
-    // 마이페이지 에서 회원정보 수정 시
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    public static class patchRequest {
-        public String email;
-        public String password;
-        public String nickName;
-        public String phone;
-        public String name;
-
-        public Member patchRequestToEntity() {
-            return Member.builder()
-                    .email(this.email)
-                    .password(this.password)
-                    .nickName(this.nickName)
-                    .phone(this.phone)
-                    .name(this.name)
-                    .build();
-        }
-
-    }
-
-
-
-
 }
