750569972b0a084643d110b3428d83d1ce1561d2
chanhyeok <gord10011@naver.com>
2024-01-16 17:59:51 +0900

    dev-be pull

---
+++ src/main/java/com/pair/boardspring/image/entity/ImageEntity.java
@@ -0,0 +1,17 @@
+//package com.pair.boardspring.image.entity;
+//
+//import com.pair.boardspring.freeboard.entity.BoardEntity;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.ManyToOne;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class ImageEntity {
+//    @Id
+//    @ManyToOne
+//    private BoardEntity boardEntity;
+//}
