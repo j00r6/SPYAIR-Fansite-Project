aa92cfe1bc182252efa50e04215e3dc9cb1615ea
j00r6 <jindory8@gmail.com>
2024-01-18 15:29:49 +0900

    Fix : Exception Logic

--- backend/src/main/java/pair/boardspring/security/userdetails/CustomUserDetailsService.java
+++ backend/src/main/java/pair/boardspring/security/userdetails/CustomUserDetailsService.java
@@ -9,8 +9,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import pair.boardspring.exception.BusinessLogicException;
-import pair.boardspring.exception.ExceptionCode;
+import pair.boardspring.exception.BadRequestException;
+//import pair.boardspring.exception.BusinessLogicException;
+//import pair.boardspring.exception.ExceptionCode;
 
 import pair.boardspring.member.entity.Member;
 import pair.boardspring.member.repository.MemberRepository;
@@ -28,7 +29,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Member> optionalMember = repository.findByEmail(username);
-        Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        Member findMember = optionalMember.orElseThrow(() -> new BadRequestException("존재하지 않는 회원정보 입니다!"));
         log.info("로그인한 멤버의 멤버 ID " + findMember.getMemberId().toString());
 
         Collection<? extends GrantedAuthority> authorities = findMember.getRoles().stream()
