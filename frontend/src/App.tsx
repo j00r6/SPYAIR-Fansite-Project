2f45297e3d0c2ebe00d292285ec8520c6a3bbd16
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-05 15:36:36 +0900

    refact:App.tsx 파일 코드 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
+
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer";
+
 import MainPage from "./pages/MainPage.tsx";
 import Profile from "./pages/ProfilePage";
-import GlobalStyle from "./styles/GlobalStyles";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import FreeBoardListPage from "./pages/FreeBoardListPage";
@@ -12,7 +14,8 @@ import FreeBoardEditPage from "./pages/FreeBoardEditPage";
 import NoticeListPage from "./pages/NoticeListPage";
 import NoticeDetailPage from "./pages/NoticeDetailPage";
 import NoticeEditPage from "./pages/NoticeEditPage";
-import Footer from "./components/Footer";
+
+import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
@@ -46,10 +49,12 @@ function App() {
 export default App;
 
 const Container = styled.div`
-  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Main = styled.div`
+const Main = styled.main`
   width: 100%;
   flex: 1;
   display: flex;
